60c506fe112d8491d174c9b71aaf886887ca6f3d
JJ Espinoza <espin086@gmail.com>
2016-09-05 20:23:15 -0700

    save

--- 2. Descriptive Analytics/How many groups of actors are there?/1. Clustering of Actors by Salary.R
+++ 2. Descriptive Analytics/How many groups of actors are there?/1. Clustering of Actors by Salary.R
@@ -1,22 +1,31 @@
+#########################################
+#Data visualization of raw data
+hist(df$Total.Gross)
+hist(df$Num.Movies)
+hist(df$Avg.Per.Movie, breaks = 20)
+hist(df$Gross.of.No.1.Movie, breaks = 20)
+
+pairs(~Total.Gross+Num.Movies+Avg.Per.Movie + Gross.of.No.1.Movie, data = df,
+      main="Simple Scatterplot Matrix", pch=21, bg="red")
 
 #########################################
 #Preparing data for cluster analysis
 
 row.names(df) <- df[[1]] #make the name the row.name
-df <- df[c(-1,-4:-6)] #Removing text of number 1 movie
+df <- df[c(-1,-5)] #Removing text of number 1 movie
 df <- df[complete.cases(df),]
 df <- scale(df, center = TRUE, scale = TRUE)#center and scale before clustering to ensure each variable is weighted equally in clustering exercise
 
 ##############################################
 # Determine number of clusters
 wss <- (nrow(df)-1)*sum(apply(df,2,var))
-for (i in 2:15) wss[i] <- sum(kmeans(df, 
+for (i in 2:10) wss[i] <- sum(kmeans(df, 
                                      centers=i)$withinss)
 
-plot(1:15, wss, type="b", xlab="Number of Clusters",
-     ylab="Within groups sum of squares")
+plot(1:10, wss, type="b", xlab="Number of Clusters",
+     ylab="Within groups sum of squares", col = "red")
 
-num.clust <- 4
+num.clust <- 5
 
 ##############################################
 # Determine number of clusters
@@ -43,4 +52,13 @@ df <- data.frame(df, groups)
 ##############################################
 #Cluster visualizations
 
-plot(df$Total.Gross, df$Num.Movies, col=df$fit1.cluster)
+pairs(~Total.Gross+Num.Movies+Avg.Per.Movie + Gross.of.No.1.Movie, data = df,
+      main="Simple Scatterplot Matrix",pch = 21, bg = df$fit1.cluster)
+
+
+##############################################
+#Example stars
+
+df <- df[order(-df$Total.Gross),]
+temp <- df[which(df$fit1.cluster == 5),]
+
